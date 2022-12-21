26ddc47cccf8c381436167a6da92088e59f3d423
Vitali <vitali.75@mail.ru>
2022-12-21 17:49:53 +0300

    Добавил код решения задачи!

---
+++ zadacha30/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] arrayOne = new string[6] {"59684", "777", "hello", "world", "-2", ":-)"};
+string[] arrayTwo = new string[arrayOne.Length];
+void SecondArrayWithIF(string[] arrayOne, string[] arrayTwo)
+{
+    int count = 0;
+    for (int i = 0; i < arrayOne.Length; i++)
+    {
+        if(arrayOne[i].Length <= 3)
+        {
+            arrayTwo[count] = arrayOne[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(arrayOne, arrayTwo);
+PrintArray(arrayTwo);
